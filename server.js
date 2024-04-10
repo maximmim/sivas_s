2426e7a61ee8fa595617e1f3113796f99947e728
maximmim <93773088+maximmim@users.noreply.github.com>
2024-04-10 17:40:57 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const getAllData = require('./db_conect');
 const API_KEY_BOT = '5312847705:AAE0ii_TUhEeuNPRV52iiFmB0bsEInhANt4';
 const cors = require('cors')
 const { MongoClient, ObjectId } = require('mongodb');
-const webpush = require("web-push");
 const crypto = require('crypto');
 const secretKey = 'dWLO)(CGEVery'; 
 function encrypt(text, secretKey) {
@@ -81,29 +80,39 @@ async function push_db(data,s) {
   await client.connect();
   await collection.insertOne(data);
 }
-app.post('/post_data',async (req,res) =>{  
-  res.send(200,"send")
-  push_db(req.body,posts);
+app.post('/post_data', async (req, res) => {  
+  // Отправляем ответ клиенту сразу, чтобы подтвердить получение данных
+  res.status(200).send("Data received");
 
-})
-app.post('/check', (req, res) => {
+  // После этого добавляем данные в базу данных
+  try {
+    await push_db(req.body, posts);
+    console.log('Data added to database successfully');
+  } catch (error) {
+    console.error('Error while adding data to database:', error);
+    // Если произошла ошибка при добавлении данных в базу данных, вы можете отправить соответствующий ответ клиенту
+    res.status(500).send("Error occurred while adding data to database");
+  }
+});
+
+app.post('/check', async (req, res) => {
   let y = req.body;
-  getAllData(users).then((users)=>{
-      users.map((h)=>{
-        if (y.pass == decrypt(h.password,secretKey)) {
-          res.send({
-            status:true
-          })
-        } 
-        else {
-          res.send({
-            status:false
-          })
-        }
-      })
-  })
 
-})
+    const d = await getAllData(users);
+    let isPasswordCorrect = false;
+
+    for (const user of d) {
+      if (y.pass === decrypt(user.password, secretKey)) {
+        isPasswordCorrect = true;
+        break; // Прерываем выполнение цикла, так как пароль найден
+      }
+    }
+
+    // Отправляем ответ клиенту в зависимости от результата проверки пароля
+    res.json({ status: isPasswordCorrect });
+  
+});
+
 
 app.post("/login", async (req,res)=>{
 let user = req.body;
@@ -114,7 +123,55 @@ res.send(200,"send")
 
 })
 
+const webPush = require('web-push');
+const PUBLIC_KEY = 'BASbtuZCuVdXUMLM5By1Vw5Z_gkx_llfbv9ll0jPIsdqPYpefWlBPIsk7cs1OT-YhN0baqNODw1w_7Ac8aHg-CE'
+const PRIVAT_KEY = '8e4fcsAYwnj2VJBTUMIRD4Br20W1sLHiXmjs9KxKAnw'
+webPush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  PUBLIC_KEY,
+  PRIVAT_KEY
+);
 
+// Инициализация объекта для хранения подписок
+let subscriptions = {}
+
+// Роут для подписки на push-уведомления
+app.get('/sub', (req, res) => {
+  // Извлекаем подписку и ID из запроса
+  const sub = req.body;
+  return res.status(201).json({js:sub});
+});
+// Роут для подписки на push-уведомления 
+app.post('/subscribe', (req, res) => {
+    // Извлекаем подписку и ID из запроса
+    const {subscription, id} = req.body;
+    // Сохраняем подписку в объекте под ключом ID
+    subscriptions[id] = subscription;
+    // Возвращаем успешный статус
+    console.log(subscriptions)
+    return res.status(201).json({data: {success: true}});
+});
+
+// Роут для отправки push-уведомлений
+app.post('/send', (req, res) => {
+    // Извлекаем сообщение, заголовок и ID из запроса
+    const {message, title, id} = req.body;
+    // Находим подписку по ID
+    const subscription = subscriptions[id];
+    // Формируем payload для push-уведомления
+    const payload = JSON.stringify({ title, message });
+    
+    // Отправляем push-уведомление
+    webPush.sendNotification(subscription, payload)
+    .catch(error => {
+        // В случае ошибки возвращаем статус 400
+        return res.status(400).json({data: {success: false}});
+    })
+    .then((value) => {
+        // В случае успешной отправки возвращаем статус 201
+        return res.status(201).json({data: {success: true}});
+    });
+});
 
 //bot.on('text', async msg => {
 //    async function send(h) {
@@ -131,44 +188,6 @@ res.send(200,"send")
 //})
 
 
-function send_message() {
-
-
-const VAPID = {
-  publicKey:
-  "BKI6Pt0oowfWlpubOmOMUMgl-YrBK6xvv08kzHcdKI8X0xYLVXI4jdkBbgYfGAZ1nO-6P1aXAA1kzmUfWEq7loE",
-  privateKey: 'aLXoqFnqA_Q3KZyr0Jslt-c7QA1wkGgh2BDHou9y4Og',
-};
-webpush.setVapidDetails(
-  "mailto:example@yourdomain.org",
-  VAPID.publicKey,
-  VAPID.privateKey
-);
-
-  async function sendNotification(pushSubscription) {
-  try {
-    return await webpush
-      .sendNotification(
-        pushSubscription,
-        "JUST TEXT"
-      );
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-getAllData(users).then((f)=>{
-  f.forEach(item => {
-
-sendNotification(item);
-
-  })
-})
-
-}
-
-
-//send_message()
 
 
 
